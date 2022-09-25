109a49cdfbf45c4c8c8ae07cbf89b25c90fe1815
suisoh <88411618+ssuish@users.noreply.github.com>
2022-09-25 22:10:51 +0800

    final rps

--- projects/py12/src/rps.py
+++ projects/py12/src/rps.py
@@ -8,6 +8,16 @@ def play():
     if user == computer:
         return('tie')
     
+    if is_win(user, computer):
+        return 'You won!'
+    
+    return 'You lost!'
+    
 def is_win(player, opponent):
-    pass
+    # return true if player wins
+    # r > s, s > p , p > r
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p')\
+        or (player =='p' and opponent == 'r'):
+            return True
 
+print(play())
