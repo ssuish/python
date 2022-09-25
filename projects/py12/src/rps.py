d930fdb9632a7fc5282a9803d7f64b8e6fb5c74a
suisoh <88411618+ssuish@users.noreply.github.com>
2022-09-25 21:39:28 +0800

    new

--- projects/py12/src/rps.py
+++ projects/py12/src/rps.py
@@ -1,2 +1,13 @@
 # Rock, Paper, Scissors game
+import random
+
+def play():
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors: ")
+    computer = random.choice(['r', 'p', 's'])
+    
+    if user == computer:
+        return('tie')
+    
+def is_win(player, opponent):
+    pass
 
