4b0e42f4e8add7148bf84321d5bd3e797c85fe0c
suisoh <88411618+ssuish@users.noreply.github.com>
2022-12-20 21:46:15 +0800

    day 2

---
+++ problems/avc-2022/day2.py
@@ -0,0 +1,8 @@
+def Main():
+    path = "D:\\dev\\ssuish-repo\\python\\problems\\avc-2022\\data\\day1.txt"
+      
+    with open(path, 'r') as file:
+        pass
+
+if __name__ == '__main__':
+    Main()
