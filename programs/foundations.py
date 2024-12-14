9922f1c116edf82ceb0718a0666960f4bd2a6ff2
suisoh <88411618+ssuish@users.noreply.github.com>
2024-12-14 10:27:33 +0800

    added exercise files

--- programs/foundations.py
+++ programs/foundations.py
@@ -1,2 +1,28 @@
 # Python Foundations
 
+# List
+# a simple collection that groups pieces of data together in a certain order
+city = ["Tokyo", "Dakar", "Mumbai", "Buenos Aires"]
+# print(city[0])
+
+# Dictionary
+# stores related information
+food = {
+    'appetizer': 'hummus'
+    }
+# print(food["appetizer"])
+
+# Challenge 1
+star = ['Sol', 'Alpha Centauri', 'Barnard', 'Wolf 359']
+print(star[3])
+
+peaks = {
+    'African' : 'Kilimanjaro',
+    'Antarctic' : 'Vinson',
+    'Australian' : 'Puncak Jaya',
+    'Eurasian' : 'Everest',
+    'North_American' : 'Denali',
+    'Pacific' : 'Mauna Kea',
+    'South_American' : 'Aconcagua'
+}
+print(peaks["Pacific"])
